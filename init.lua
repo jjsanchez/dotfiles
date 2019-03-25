d0402a0f26ee845502e1e4d6c79e0a461fa4fbe2
Jaime Sanchez <jjsg@amazon.com>
2019-03-25 11:10:20 +0100

    Adding flux

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require "modules/layouts/layouts"
---require "modules/pomo/pomo"
+local flux = require "modules/flux/flux"
 
+--require "modules/pomo/pomo"
 --pomo()
 
 function reloadConfig(files)
@@ -17,3 +18,7 @@ function reloadConfig(files)
 end
 local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
+
+local bigMash = {"cmd", "ctrl", "alt"}
+hs.hotkey.bind(bigMash, "-", flux.decreaseLevel)
+hs.hotkey.bind(bigMash, "=", flux.increaseLevel)
